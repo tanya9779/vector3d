eb886ab2885f855d190ad4edfdb154ce33f8ae8d
Alexey Ermakov <ermakov.as@mipt.ru>
2016-10-19 21:58:04 +0300

    Первый коммит

---
+++ vector3d.cpp
@@ -0,0 +1,10 @@
+#include "vector3d.hpp"
+
+#include <cmath>
+
+const vector3d vector3d::X = {1, 0, 0};
+const vector3d vector3d::Y = {0, 1, 0};
+const vector3d vector3d::Z = {0, 0, 1};
+const vector3d vector3d::ZERO = {0, 0, 0};
+
+// TODO: дописать
---
+++ vector3d.hpp
@@ -0,0 +1,19 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+
+#include <ostream>
+
+class vector3d {
+public:
+    const static vector3d X;
+    const static vector3d Y;
+    const static vector3d Z;
+    const static vector3d ZERO;
+
+    // TODO: дописать
+
+};
+
+// TODO: дописать
+
+#endif
